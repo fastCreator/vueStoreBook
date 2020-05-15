cad026a7efdcccab5c7d4a86289154029f3ef4b7
zhangguo <zhangguo@inke.cn>
2020-05-15 19:31:18 +0800

    storey book

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,13 @@ const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin')
 module.exports = {
   stories: ['../stories/**/*.stories.(js|mdx)'],
   addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs',
-    '@storybook/addon-docs/register',
-    '@storybook/addon-backgrounds/register',
+    // https://github.com/storybookjs/storybook/tree/master/addons
+    '@storybook/addon-actions', // 事件响应
+    '@storybook/addon-links', // 链接跳转
+    '@storybook/addon-knobs', // 参数联动
+    '@storybook/addon-docs/register', // 文档
+    '@storybook/addon-backgrounds/register', // 背景色
+    '@storybook/addon-viewport/register', // 视图  /viewport
     {
       name: '@storybook/addon-docs',
       options: {
---
+++ README.md
@@ -0,0 +1,10 @@
+#### 在线访问
+
+https://fastcreator.github.io/vueStoreBook/
+
+
+#### 开发流程
+1. 在stories中按功能分类、添加组件包
+2. index.stories.mdx 中写demo和文档
+3. src目录中写组件
+4
