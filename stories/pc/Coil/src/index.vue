b695d4e720156f58fa813beb13ac651b4b6e5e01
zhangguo <237625092@qq.com>
2020-05-27 15:07:10 +0800

    打点

--- stories/pc/Coil/src/index.vue
+++ stories/pc/Coil/src/index.vue
@@ -5,26 +5,42 @@
     version="1.1"
     :width="width"
     :height="height"
+    @mousedown="drawBlock"
     @mousemove="handlerMousemove"
-    @click="handlerClick"
+    :style="{backgroundImage:`url(${src})`}"
   >
     <g
       class="block"
       v-bind="pathAttrs"
       v-for="(it, i) in blocks"
+      :fill="fillBlock(it)"
       :key="it.key"
     >
+      <template v-if="it.type === 'dot'">
+        <circle
+          v-bind="dotAttrs.circle"
+          :cx="it.paths[0]"
+          :cy="it.paths[1]"
+        ></circle>
+        <text
+          v-bind="dotAttrs.text"
+          :x="it.paths[0]"
+          :y="it.paths[1]+1"
+          text-anchor="middle"
+          dominant-baseline="middle"
+        >{{i+1}}</text>
+      </template>
       <ellipse
-        v-if="it.type === 'ellipse'"
+        v-else-if="it.type === 'ellipse'"
         v-bind="getEllipsePath(it)"
-        @mousedown="handlerMousedown($event, i)"
+        @mousedown.stop="handlerMousedown($event, i)"
       />
       <path
         v-else
-        @mousedown="handlerMousedown($event, i)"
+        @mousedown.stop="handlerMousedown($event, i)"
         :d="getRectPath(it)"
       />
-      <g v-if="selectedI === i">
+      <g v-if="selectedI === i && it.type!=='dot'">
         <rect
           v-for="(jt, j) in getShowPath(it)"
           :x="jt[0] - rectAttrs.width / 2"
@@ -32,7 +48,7 @@
           :key="`${i},${j}`"
           v-bind="rectAttrs"
           :class="jt.join(',') + it.square + j"
-          @mousedown="handlerMousedown($event, i, j)"
+          @mousedown.stop="handlerMousedown($event, i, j)"
         ></rect>
       </g>
     </g>
@@ -58,47 +74,102 @@ import mouse from './lib/mouse'
 export default {
   name: 'Coil',
   props: {
+    dotAttrs: {
+      default: () => ({
+        circle: {
+          stroke: 'none',
+          strokeWidth: 0,
+          fill: 'red',
+          r: 8
+        },
+        text: {
+          fill: 'black',
+          'font-size': '10px'
+        }
+      })
+    },
     pathAttrs: {
       default: () => ({
-        stroke: '#cccccc',
+        stroke: 'rgb(178,174,171)',
         strokeWidth: 1,
-        fill: 'rgba(25,225,100,0.5)'
+        fill: 'rgba(255,225,255,0.4)'
       })
     },
     rectAttrs: {
       default: () => ({
-        width: 10,
-        height: 10,
-        style: 'fill:rgb(0,0,255);stroke-width:1;stroke:rgb(0,0,0)'
+        width: 8,
+        height: 8,
+        style: 'fill:rgb(255,255,255);stroke-width:1;stroke:rgb(0, 0, 0)'
       })
     },
-    width: {
-      default: '100%'
-    },
-    height: {
-      default: '100%'
+    src: {
+      required: true
     }
   },
   mixins: [history, keyboard, mouse],
   data () {
     return {
+      width: 0,
+      height: 0,
       key: 0,
       selectedI: null,
       blocks: [],
-      copyBlock: null
+      copyBlock: null,
+      state: null,
     }
   },
   methods: {
+    fillBlock (block) {
+      if (!block.waitClose || block.type !== 'polygon') {
+        return this.pathAttrs.fill
+      } else {
+        return 'none'
+      }
+    },
+    drawBlock (e) {
+      const type = this.state
+      const { offsetX, offsetY } = e
+      if (type === 'rect' || type === 'ellipse') {
+        this.$addBlock(this.state, [[offsetX, offsetY], [offsetX, offsetY], [offsetX, offsetY], [offsetX, offsetY]])
+        const i = this.blocks.length - 1
+        this.handlerMousedown(e, i, 2)
+      } else if (type === 'polygon') {
+        const block = this.blocks[this.blocks.length - 1]
+        if (block && block.waitClose) {
+          this.addHorn(e)
+        } else {
+          this.$addBlock(this.state, [[offsetX, offsetY]], { waitClose: true })
+        }
+      } else if (type === 'dot') {
+        this.$addBlock(this.state, [offsetX, offsetY])
+      }
+    },
+    $setState (state) {
+      this.state = state
+    },
+    loadImg () {
+      const img = new Image()
+      img.src = this.src
+      img.onload = (e) => {
+        this.width = img.width
+        this.height = img.height
+      }
+    },
     // 设置当前所有展示
-    $setBlocks (blocks) {
+    $setBlocks (history) {
+      if (!history) {
+        return false
+      }
+      const { blocks, selectedI } = history
       if (blocks) {
         this.blocks = blocks
       }
+      this.selectedI = selectedI
     },
     // 添加一个块
-    $addBlock (type, paths) {
-      this.blocks.push({ key: this.key++, type, paths })
-      this.addHistory()
+    $addBlock (type, paths, data) {
+      this.blocks.push({ key: this.key++, type, paths, ...data })
+      this.selectedI = this.blocks.length - 1
     },
     // 模块上下左右移动
     $blockMove (direc) {
@@ -152,13 +223,17 @@ export default {
         block.paths.push([e.offsetX, e.offsetY])
       }
     },
-    handlerClick (e) {
-      if (e.target.tagName === 'svg') {
-        this.addHorn(e)
-      }
-    }
+    // handlerClick (e) {
+    //   if (e.target.tagName === 'svg') {
+    //     this.addHorn(e)
+    //   }
+    // }
   },
-  async created () { },
+  beforeCreate () {
+  },
+  async created () {
+    this.loadImg()
+  },
   mounted () {
   }
 }
