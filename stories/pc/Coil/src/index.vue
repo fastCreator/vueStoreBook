c96e243f5ed1da84f002c4f3d323ae804643c9e0
zhangguo <237625092@qq.com>
2020-05-27 18:09:50 +0800

    标记点

--- stories/pc/Coil/src/index.vue
+++ stories/pc/Coil/src/index.vue
@@ -16,20 +16,12 @@
       :fill="fillBlock(it)"
       :key="it.key"
     >
-      <template v-if="it.type === 'dot'">
-        <circle
-          v-bind="dotAttrs.circle"
-          :cx="it.paths[0]"
-          :cy="it.paths[1]"
-        ></circle>
-        <text
-          v-bind="dotAttrs.text"
-          :x="it.paths[0]"
-          :y="it.paths[1]+1"
-          text-anchor="middle"
-          dominant-baseline="middle"
-        >{{i+1}}</text>
-      </template>
+      <circle
+        v-if="it.type === 'dot'"
+        v-bind="dotAttrs"
+        :cx="it.paths[0]"
+        :cy="it.paths[1]"
+      ></circle>
       <ellipse
         v-else-if="it.type === 'ellipse'"
         v-bind="getEllipsePath(it)"
@@ -40,6 +32,23 @@
         @mousedown.stop="handlerMousedown($event, i)"
         :d="getRectPath(it)"
       />
+
+      <text
+        v-if="it.type === 'dot'"
+        v-bind="textAttrs"
+        :x="it.paths[0]"
+        :y="it.paths[1]+1"
+        text-anchor="middle"
+        dominant-baseline="middle"
+      >{{i+1}}</text>
+      <text
+        v-else
+        v-bind="textAttrs"
+        :x="coordinateXX(it)+4"
+        :y="coordinateXY(it)+4"
+        text-anchor="start"
+        dominant-baseline="hanging"
+      >{{i+1}}</text>
       <g v-if="selectedI === i && it.type!=='dot'">
         <rect
           v-for="(jt, j) in getShowPath(it)"
@@ -76,16 +85,16 @@ export default {
   props: {
     dotAttrs: {
       default: () => ({
-        circle: {
-          stroke: 'none',
-          strokeWidth: 0,
-          fill: 'red',
-          r: 8
-        },
-        text: {
-          fill: 'black',
-          'font-size': '10px'
-        }
+        stroke: 'none',
+        strokeWidth: 0,
+        fill: 'red',
+        r: 8
+      })
+    },
+    textAttrs: {
+      default: () => ({
+        stroke: 'black',
+        'font-size': '10px'
       })
     },
     pathAttrs: {
@@ -223,6 +232,20 @@ export default {
         block.paths.push([e.offsetX, e.offsetY])
       }
     },
+    coordinateX ({ paths, type }) {
+      if(type === 'polygon'){
+        return paths[0][0]
+      }
+      const arrX = paths.map(it => it[0])
+      return Math.min.apply(null, arrX)
+    },
+    coordinateY ({ paths, type }) {
+      if(type === 'polygon'){
+        return paths[0][1]
+      }
+      const arrY = paths.map(it => it[1])
+      return Math.min.apply(null, arrY)
+    },
     // handlerClick (e) {
     //   if (e.target.tagName === 'svg') {
     //     this.addHorn(e)
@@ -241,6 +264,7 @@ export default {
 
 <style lang="less">
 .coil {
+  user-select: none;
   .block {
     rect {
       cursor: pointer;
